33bf3b78182a4df5718cfc5d68c1b64bf18d821f
santam85 <santa.85@gmail.com>
2021-08-05 15:17:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng2-charts-pie-template-3vshf5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng2-charts-pie-template-3vshf5)
---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType, ChartOptions } from 'chart.js';
+import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  // Pie
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
+  public pieChartData: SingleDataSet = [300, 500, 100];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
+  constructor() {
+    monkeyPatchChartJsTooltip();
+    monkeyPatchChartJsLegend();
+  }
+
+  ngOnInit() {
+  }
+}
